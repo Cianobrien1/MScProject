c69b69e8da61f45115f45214e7667cf33853034e
Cianobrien1 <cianobrien1010@gmail.com>
2023-06-07 23:45:09 +0000

    .

---
+++ Redundant _code/openbabel_smile_to_pdb.py
@@ -0,0 +1,25 @@
+from openbabel import pybel
+import os
+from openbabel import openbabel as ob
+
+smile_file = "/home/s2451611/MScProject/Broken_Decoy_Smiles.txt"
+i = 0
+os.mkdir("openbabel_dir")
+with open(smile_file) as file:
+    for eachline in file.readlines():
+        splitline = eachline.split()
+        smile = splitline[6]
+        i += 1
+        # Create a molecule from the SMILES string
+        mol = pybel.readstring("smi", smile)
+
+        # Convert to 3D. This is equivalent to EmbedMolecule in RDKit.
+        mol.make3D()
+
+        # If you want to output the molecule in a particular format (e.g., PDB), you can do so:
+        pdb_output = mol.write("pdb")
+        with open(f"openbabel_dir/{i}.pdb", "w") as f:
+            f.write(pdb_output)
+
+    
+    
