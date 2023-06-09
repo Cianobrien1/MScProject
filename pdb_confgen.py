40ce547875382ca93fd034e57e63efa06bcc4742
Cianobrien1 <cianobrien1010@gmail.com>
2023-06-09 13:51:53 +0000

    Updated confgen command to generate 20 conformers

--- pdb_confgen.py
+++ pdb_confgen.py
@@ -8,7 +8,7 @@ input_dir = "/home/s2451611/MScProject/pdb_to_mols_dir/"
 def confgen(file_arg):
     output_file_name = os.path.basename(file_arg).rstrip(".mol") + ".sdf"
     output_file = output_dir + output_file_name
-    os.system(f"confgen -i {file_arg} -o {output_file}")
+    os.system(f"confgen -i {file_arg} -o {output_file} --numconf 20")
 
 def main():
     os.makedirs(output_dir, exist_ok=True)
