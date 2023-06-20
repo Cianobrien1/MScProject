31e25d06df1b3b59d5de0fe52e1c32f453f72b6f
Cianobrien1 <cianobrien1010@gmail.com>
2023-06-20 16:03:29 +0000

    .

---
+++ SMILE_1yet.py
@@ -0,0 +1,19 @@
+from rdkit import Chem
+
+input_file = "/home/s2451611/MScProject/1yet/1yet_ligand.mol2"
+
+def generate_smiles_from_mol2(filename):
+    suppl = Chem.MolFromMol2File(filename, sanitize=False)
+    smiles = Chem.MolToSmiles(suppl)
+    return smiles
+
+def save_to_file(smile):
+    with open("output.smi", "w") as f:
+        f.write(f"{smile}\n")
+    return
+
+def main():
+    save_to_file(generate_smiles_from_mol2(input_file))
+    
+if __name__ == "__main__":
+    main()
