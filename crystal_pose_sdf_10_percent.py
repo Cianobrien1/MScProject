9e067d7136dbe8ff0beb9fd4c55acdeee84491fe
Cianobrien1 <cianobrien1010@gmail.com>
2023-06-25 18:33:15 +0000

    .

---
+++ crystal_pose_sdf_10_percent.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+# Define directories
+src_dir = "/home/s2451611/MScProject/openbabel_sdf_crystal_pose_ligands"
+dest_dir = "/home/s2451611/MScProject/10_percent_crystal_ligand_sdf"
+test_sample_dir = "/home/s2451611/MScProject/10_percent_alignment_test_sample"
+
+# Create destination directory if it doesn't exist
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+
+# Get list of file names in the test_sample_dir
+test_sample_files = os.listdir(test_sample_dir)
+test_sample_ids = [file.split('_')[0] for file in test_sample_files if file.endswith('.sdf')]
+
+# Iterate over the files in source directory
+for file_name in os.listdir(src_dir):
+    # Check if the file ends with .sdf
+    if file_name.endswith('.sdf'):
+        # Get the pdb_id from the file_name
+        pdb_id = file_name.split('_')[0]
+        # If the pdb_id exists in the test_sample_ids, copy the file
+        if pdb_id in test_sample_ids:
+            # Define source and destination paths
+            src_file_path = os.path.join(src_dir, file_name)
+            dest_file_path = os.path.join(dest_dir, file_name)
+            
+            # Copy the file
+            shutil.copy2(src_file_path, dest_file_path)
+
+print("File copying completed.")
