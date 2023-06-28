cc54981cae9fcaaea606648ffd711a5d7c61226b
Cianobrien1 <cianobrien1010@gmail.com>
2023-06-28 00:47:23 +0000

    .

---
+++ pymol_alignment.py
@@ -0,0 +1,43 @@
+import os
+import glob
+from pymol import cmd
+
+# Directories
+input_dir1 = "/home/s2451611/MScProject/10_percent_alignment_test_sample"
+input_dir2 = "/home/s2451611/MScProject/10_percent_crystal_ligand_sdf"
+output_dir = "aligned_pdbs_pymol"
+
+# Create output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
+
+# Output RMSD file
+rmsd_file = "pymol_avg_rmsd.txt"
+
+def align_and_save():
+    all_rmsd_list = []
+    for file1 in glob.glob(os.path.join(input_dir1, '*.sdf')):
+        pdb_id = os.path.basename(file1).split('_')[0]
+        file2 = os.path.join(input_dir2, f"{pdb_id}_ligand.sdf")
+        
+        if not os.path.exists(file2):
+            print(f"No corresponding ligand sdf for {pdb_id}")
+            continue
+        
+        cmd.reinitialize()
+        cmd.load(file1, 'conformers')
+        cmd.load(file2, 'ligand')
+        
+        for state in range(1, cmd.count_states('conformers')+1):
+            rmsd = cmd.align(f'conformers and state {state}', 'ligand')[0]
+            all_rmsd_list.append(rmsd)
+        
+        # get the state with minimum rmsd
+        min_rmsd_state = all_rmsd_list.index(min(all_rmsd_list)) + 1
+        cmd.save(os.path.join(output_dir, f"{pdb_id}_aligned.pdb"), f'conformers and state {min_rmsd_state}')
+    
+    # write average rmsd to file
+    with open(rmsd_file, 'w') as f:
+        f.write(f"Average RMSD: {sum(all_rmsd_list) / len(all_rmsd_list)}\n")
+
+if __name__ == "__main__":
+    align_and_save()
